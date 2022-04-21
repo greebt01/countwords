4974ee45479feaa8331532be11906bf7894b5153
Birk <rgeebt01@yahoo.com>
2022-04-21 16:22:14 -0400

    added determination of last word and letter count of last word

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Program that counts the number of words in text a user gives it.
 # Update
 Added string to array method, and all methods are disabled by default.
 
+# Update2
+Added a section to find the last word in the text that a user gives it, and count the number of letters in that last word.
+
--- src/main/java/CountWords.java
+++ src/main/java/CountWords.java
@@ -6,6 +6,7 @@ public class CountWords {
         Scanner scanner=new Scanner(System.in);
         System.out.println("Enter text whose words you want to count");
         String targetString=scanner.nextLine();
+        CountWords.lastString(targetString);
 //        CountWords.StringToCharArrayExample(targetString);
 //        CountWords.wordCountTokenizer(targetString);
 //        CountWords.stringWordCount(targetString);
@@ -41,10 +42,20 @@ public class CountWords {
     }
 
     public static void StringToCharArrayExample(String targetString) {
-        ;
         char[] ch = targetString.toCharArray();
         for (int i = 0; i < ch.length; i++) {
             System.out.println(ch[i]);
         }
+    }
+
+    public static void lastString(String targetString) {
+        var lastWord= targetString.substring(targetString.lastIndexOf(' '), targetString.length());
+        System.out.println("Last word of the text is: "+lastWord);
+        int count = 0;
+        for (int i = 0; i < lastWord.length(); i++) {
+            if (Character.isLetter(lastWord.charAt(i)))
+                count++;
+        }
+        System.out.println("Letters: "+count);
     }
 }
